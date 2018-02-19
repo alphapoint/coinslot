50b9d4a134f6f607880dcf89d224220407795f5b
Kiryl Stasevich <kirylstasevich@gmail.com>
2018-02-19 15:50:25 +0300

    getAddressType function

---
+++ src/addressValiator.js
@@ -0,0 +1,28 @@
+import cryptoUtils from './cryptoUtils';
+import base58 from './base58';
+
+export default {
+  getAddressType(address) {
+    let decoded;
+
+    try {
+      decoded = base58.decode(address);
+    } catch (e) {
+      // if decoding fails, assume invalid address
+      return null;
+    }
+
+    const length = decoded.length;
+
+    // should be 25 bytes per btc address spec
+    if (length !== 25) {
+      return null;
+    }
+
+    const checksum = cryptoUtils.toHex(decoded.slice(length - 4, length));
+    const body = cryptoUtils.toHex(decoded.slice(0, length - 4));
+    const goodChecksum = cryptoUtils.sha256(cryptoUtils.sha256(body)).substr(0, 8);
+
+    return checksum === goodChecksum ? cryptoUtils.toHex(decoded.slice(0, 1)) : null;
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import base58 from './base58';
+import validator from './addressValiator';
 
-export default function () {
-  const obj = {a: '123'};
-  const {a} = obj;
-  return base58.decode(a);
+export default function foo() {
+  const address1 = '12KYrjTdVGjFMtaxERSk3gphreJ5US8aUP';
+  
+  console.log(validator.getAddressType(address1));
 }
+
+foo();
+
+export const addressValidator = validator;
