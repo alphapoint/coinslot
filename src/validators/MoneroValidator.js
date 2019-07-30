72ca0fb4976cc5d5f7d92c79c665e30f7165a510
joshuarossi <joshua.rossi@icloud.com>
2019-07-30 18:49:11 +0900

    AS-11909 Monero Integrated Addresses

--- src/validators/MoneroValidator.js
+++ src/validators/MoneroValidator.js
@@ -6,8 +6,9 @@ import SUPPORTED_CURRENCIES from '../supportedCurrencies';
  */
 export default class MoneroValidator extends RegExpValidator {
   constructor() {
-    super([
-      SUPPORTED_CURRENCIES.monero,
-    ], /^4([0-9]|[A-B])([0-9a-zA-Z]){93}$/);
+    super(
+      [SUPPORTED_CURRENCIES.monero],
+      /^4([0-9]|[A-B])([0-9a-zA-Z]){93}$|^4([0-9]|[A-B])([0-9a-zA-Z]){104}$/
+    );
   }
 }
